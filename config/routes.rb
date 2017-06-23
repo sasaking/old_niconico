0705fcb43c56aec655336591adfa960db123be29
佐々木宏明 <sasakihiroaki@sasakihiroakinoMacBook-Air.local>
2017-06-23 19:09:49 +0900

    edit views and create add common header.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   get 'main/art'
 
+  get 'main/reserves'
+
   resources :reserves
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

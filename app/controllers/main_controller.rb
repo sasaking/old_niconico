a14bb33a56b52c9d0db46465a3d870c62d49aae2
yasuhito_nakagawa <nakagawa.ablast@gmail.com>
2017-07-16 14:57:32 +0900

    move header contents from partial to layouts/application.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,24 +1,25 @@
 class MainController < ApplicationController
   def index
+    @title = "トップページ"
   end
 
   def schedule
-      @title = "スケジュール"
+    @title = "スケジュール"
   end
 
   def event
-      @title = "イベント"
+    @title = "イベント"
   end
 
   def creater
-      @title = "講師情報"
+    @title = "講師情報"
   end
 
   def art
-      @title = "アート情報"
+    @title = "アート情報"
   end
 
   def reserve
-      @title = "予約"
+    @title = "予約"
   end
 end

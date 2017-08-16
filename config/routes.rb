9f5b1cd2de3db6a5256872c87d57d6c3082499c6
佐々木宏明 <sasakihiroaki@sasakihiroakinoMacBook-Air.local>
2017-08-16 13:36:55 +0900

    edit route & reserve

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
   root 'main#index' 
-  get 'index', to: 'main#index'
-
+  get 'index',    to: 'main#index'
   get 'schedule', to: 'main#schedule'
-
-  get 'event', to: 'main#event'
-
-  get 'creater', to: 'main#creater'
-
-  get 'art', to: 'main#art'
-
-  get 'reserve', to: 'main#reserve'
+  get 'event',    to: 'main#event'
+  get 'creater',  to: 'main#creater'
+  get 'art',      to: 'main#art'
+  get 'reserve',  to: 'main#reserve'
 
   resources :reserve
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

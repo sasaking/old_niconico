ab2dcc360915d96c598e9a1b03b31db5d7a06128
佐々木宏明 <sasakihiroaki@sasakihiroakinoMacBook-Air.local>
2017-09-25 23:05:58 +0900

    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,10 @@ Rails.application.routes.draw do
   get 'art',      to: 'main#art'
   get 'reserve',  to: 'main#reserve'
 
-<<<<<<< HEAD
-  resources :reserve
-=======
   resources :reserve do
     collection do
       post 'confirm'
     end
   end
->>>>>>> editResurve
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

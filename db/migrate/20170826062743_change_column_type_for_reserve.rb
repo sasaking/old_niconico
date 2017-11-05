b669cbf40d714509a924d46f88b46e093b994826
yasuhito_nakagawa <nakagawa.ablast@gmail.com>
2017-11-05 17:59:58 +0900

    temp commit

--- db/migrate/20170826062743_change_column_type_for_reserve.rb
+++ db/migrate/20170826062743_change_column_type_for_reserve.rb
@@ -4,6 +4,6 @@ class ChangeColumnTypeForReserve < ActiveRecord::Migration[5.0]
     change_column :reserves, :tel, 'integer USING CAST(tel AS Integer)'
     change_column :reserves, :reserve_period, 'integer USING CAST(reserve_period AS Integer)'
     change_column :reserves, :beginner, 'integer USING CAST(beginner AS Integer)'
-    change_column :reserves, :question_id, 'integer USING CAST(question_id AS Integer)'
+   # change_column :reserves, :question_id, 'integer USING CAST(question_id AS Integer)'
   end
 end

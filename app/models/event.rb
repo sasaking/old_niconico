20bdb493bf6fa4f11ac86542607285bafd446c2c
佐々木宏明 <sasakihiroaki@sasakihiroakinoMacBook-Air.local>
2018-09-07 07:21:49 +0900

    admin view add Event and Event Period

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   self.primary_key = "event_id"
-  has_many :reserves, foreign_key: :event_id, primary_key: :event_id
+  has_many :reserve, foreign_key: :event_id, primary_key: :event_id
   belongs_to :reserve_period
 end
